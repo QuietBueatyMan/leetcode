ced3e41e218ccc74057c13f123a6179cd7bf025f
QuietBueatyMan <zhupenglinhaoren@163.com>
2022-01-04 15:05:21 +0800

    str convert int32

--- solution.hpp
+++ solution.hpp
@@ -444,7 +444,7 @@ namespace Solutions
             //     result = -result;
 
             // return result;
-            
+
             int32_t rev = 0;
 
             while (x != 0)
@@ -460,6 +460,75 @@ namespace Solutions
             }
 
             return rev;
+        }
+
+        /*
+        请你来实现一个 myAtoi(string s) 函数，使其能将字符串转换成一个 32 位有符号整数（类似 C/C++ 中的 atoi 函数）。
+        函数 myAtoi(string s) 的算法如下：
+        读入字符串并丢弃无用的前导空格
+        检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
+        读入下一个字符，直到到达下一个非数字字符或到达输入的结尾。字符串的其余部分将被忽略。
+        将前面步骤读入的这些数字转换为整数（即，"123" -> 123， "0032" -> 32）。如果没有读入数字，则整数为 0 。必要时更改符号（从步骤 2 开始）。
+        如果整数数超过 32 位有符号整数范围 [−231,  231 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231 − 1 的整数应该被固定为 231 − 1 。
+        返回整数作为最终结果。
+        注意：
+        本题中的空白字符只包括空格字符 ' ' 。
+        除前导空格或数字后的其余字符串外，请勿忽略 任何其他字符。
+        */
+        static int32_t myAtoi(std::string s)
+        {
+            if (s.length() == 0)
+                return 0;
+            if (s.find_first_not_of(' ') == std::string::npos)
+                return 0;
+
+            std::string trmedstr = s.substr(s.find_first_not_of(' '));
+
+            int32_t max_value = 2147483647;
+            int32_t min_value = -2147483648;
+
+            int result = 0;
+            int negtive = 1;
+
+            for (int i = 0; i < trmedstr.length(); i++)
+            {
+                if (i == 0)
+                {
+                    if (trmedstr[0] == '+')
+                    {
+                        negtive = 1;
+                        continue;
+                    }
+                    else if (trmedstr[0] == '-')
+                    {
+                        negtive = -1;
+                        continue;
+                    }
+                }
+
+                if (trmedstr[i] >= 48 && trmedstr[i] <= 57)
+                {
+
+                    int digit = trmedstr[i] - '0';
+
+                    if (result > max_value / 10)
+                        return max_value;
+                    else if (result == max_value / 10 && digit > 7)
+                        return max_value;
+                    else if (result < min_value / 10)
+                        return min_value;
+                    else if (result == min_value / 10 && digit > 8)
+                        return min_value;
+
+                    result = result * 10 + digit * negtive;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
         }
     };
 } // namespace Solutions
