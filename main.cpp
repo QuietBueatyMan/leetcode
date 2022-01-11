563d3f39b2e7a6e024d5924b56fe3fe1bf548d49
QuietBueatyMan <zhupenglinhaoren@163.com>
2022-01-11 14:22:00 +0800

    romanToInt fixed

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
     // std::vector<std::vector<int>> tmp = {{1,4},{3,5},{3,7},{9,10},{15,18},{16,20}};
     // std::vector<std::vector<int>> tmp = {{1,4},{2,3}};
     // std::vector<int> tmp = {1,3,2,5,25,24,5};
-    auto result = Solutions::Solution::intToRoman(4);
+    auto result = Solutions::Solution::romanToInt("XXIV");
     std::cout << result << std::endl;
     // for (auto item : result)
     // {
--- solution.hpp
+++ solution.hpp
@@ -667,7 +667,7 @@ namespace Solutions
         static std::string intToRoman(int num)
         {
 
-            std::map<int, std::string,std::greater<int>> symbolsmap = {
+            std::map<int, std::string, std::greater<int>> symbolsmap = {
                 {1000, "M"},
                 {900, "CM"},
                 {500, "D"},
@@ -692,6 +692,55 @@ namespace Solutions
                     num -= i.first;
                     result += i.second;
                 }
+            }
+
+            return result;
+        }
+
+        /*
+        例如， 罗马数字 2 写做 II ，即为两个并列的 1 。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+        通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+        I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+        X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+        C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+        给定一个罗马数字，将其转换成整数。
+        */
+        static int romanToInt(std::string s)
+        {
+            std::map<char, int> symbolsmap = {
+                {'M', 1000},
+                // {"CM", 900},
+                {'D', 500},
+                // {"CD", 400},
+                {'C', 100},
+                // {"XC", 90},
+                {'L', 50},
+                // {"XL", 40},
+                {'X', 10},
+                // {"IX", 9},
+                {'V', 5},
+                // {"IV", 4},
+                {'I', 1}};
+            int startindex = 0;
+            int result = 0;
+            while (startindex < s.length())
+            {
+                /* code */
+                char start_char = s[startindex];
+                if (startindex + 1 < s.length())
+                {
+                    char end_char = s[startindex + 1];
+                    if (symbolsmap[start_char] < symbolsmap[end_char])
+                    {
+                        result += (symbolsmap[end_char] - symbolsmap[start_char]);
+                        startindex += 2;
+                        continue;
+                    }
+                }
+                result += symbolsmap[start_char];
+                startindex++;
             }
 
             return result;
